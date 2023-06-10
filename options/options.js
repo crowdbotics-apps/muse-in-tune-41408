2d3bc2d03d117614a5bf4544811a6a8c127e3f3d
Crowdbotics <team@crowdbotics.com>
2023-06-10 20:18:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "muse-in-tune-41408",
+  url: "https://muse-in-tune-41408.botics.co",
+  api: "https://muse-in-tune-41408.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
